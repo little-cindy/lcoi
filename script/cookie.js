b548c1e0210c0a754f652d977afdf1cb1bfa7d10
little-cindy <78029287+little-cindy@users.noreply.github.com>
2022-03-12 15:55:21 +0800

    Create cookie.js

---
+++ script/cookie.js
@@ -0,0 +1,19 @@
+//设置cookie脚本
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+//获取cookie脚本
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return null;
+}
